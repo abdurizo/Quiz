1a22b269e8355925f09b72818d0f5eff66ce6c3e
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2026-03-02 18:39:56 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -22,11 +22,11 @@ export class AppComponent {
   total: number = 0;
   userAnswers: number[] = [];
 
-  selectAnswer(questionIndex: number, score: number) {
-    this.userAnswers[questionIndex] = score;
-  }
+  // selectAnswer(questionIndex: number, score: number) {
+  //   this.userAnswers[questionIndex] = score;
+  // }
 
   getTotalScore(): number {
-  return this.userAnswers.reduce((sum, score) => sum + (score || 0), 0);
-}
+    return this.userAnswers.reduce((sum, score) => sum + (score || 0), 0);
+  }
 }
